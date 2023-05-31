8afcd37f8542a351f4c91a0344ec80b49249b07c
SHEKHARZADE <132575635+SHEKHARZADE@users.noreply.github.com>
2023-05-31 18:32:15 +0530

    Add files via upload

---
+++ ASSIGNMENT SECOND.py
@@ -0,0 +1,64 @@
+Q. 1) How do you comment code in python? what are the different types of comments?
+
+Ans: There are two type of comment single line comments and multiline comment. we can comment in python by using sinle line by using # and and multiline comments bu using  ' ' ''
+
+Q 2) What are variables in Python? How do you declare and assign values to variables?
+Ans : In Python, variables are used to store data values. 
+
+       This is declaration .....
+        variable_name= value
+        
+        This is value assignment......
+        age=25
+        
+Q3. How do you convert one data type to another in Python?
+Ans :   In Python, you can convert one data type to another using type conversion functions.
+        
+
+a=6.77            #float value
+print(a)
+print(type(a))
+print("converted value")
+b=int(a)            #conver into integer
+print(b)
+print(type(b))
+              
+   
+Q4. How do you write and execute a Python script from the command line?
+              
+              
+Q5. Given a list my_list = [1, 2, 3, 4, 5], write the code to slice the list and obtain the sub-list [2, 3].
+Ans:    my_list=[1,2,3,4,5]
+        print(my_list[1:3])
+
+
+Q6. What is a complex number in mathematics, and how is it represented in Python?
+    Ans : Complex number 5 + j
+
+
+Q7. What is the correct way to declare a variable named age and assign the value 25 to it?
+    Ans : name_age=25
+
+
+Q8. Declare a variable named price and assign the value 9.99 to it. What data type does this variable belong to?
+    Ans : price=9.99
+          data type of price belongs to float
+          
+          
+Q9. Create a variable named name and assign your full name to it as a string. How would you print the value of this variable?
+Ans : name="SHEKHAR HARIDAS ZADE"
+
+          print(name)
+
+
+Q10. Given the string "Hello, World!", extract the substring "World".
+    Ans : string="Hello, World!"
+          print(string[7:13])
+          
+
+Q11. Create a variable named "is_student" and assign it a boolean value indicating whether you are currently a student or not.
+Ans : 
+is_student = True  # I am a student
+
+is_student = False  # I am not a student
+
